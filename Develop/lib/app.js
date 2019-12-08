1c606d65b0afbab6e2ba8a2b9d4fb107d959a8bd
TheBrellis <brianroyalellis@gmail.com>
2019-12-08 15:07:02 -0500

    started building the app.js, finished the Employee.js (passes unit test suite)

---
+++ Develop/lib/app.js
@@ -0,0 +1,62 @@
+const inquirer = require("inquirer");
+
+
+const team = [];
+
+async function initalize(role){
+  let answers1 = inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: `What is your ${role}'s name?`
+    },
+    {
+      type: "input",
+      name: "id",
+      message: `What is your ${role}'s ID?`
+    },
+    {
+      type: "input",
+      name: "email",
+      message: `What is your ${role}'s email?`
+    },
+    {
+      type: "list",
+      name: "role",
+      message:"What type of team member would you like to add?",
+      choices: ["Engineer","Intern","I don't want to add any more team members"]
+    }
+  ]);
+
+//let answers3 = {...answer1, ...answer2}
+//team.push(answers3)
+};
+
+function addMember(role){
+  if (role === "Engineer"){
+    let answers2 = inquirer.prompt([
+      {
+        type: "input",
+        name: "gitHub",
+        message: `What is your engineer's GitHub?`
+      }
+    ])
+    return answers2
+  }
+  if (role === "Intern"){
+    let answers2 = inquirer.prompt([
+    {
+      type: "input",
+      name: "school",
+      message: `What is your intern's school?`
+    }
+  ]) 
+  }
+  return
+}
+
+/* ==========================================================*/
+//START OF APP SEQUENCE 
+/* ==========================================================*/
+console.log("Please build your team");
+initalize("manager");
