9f8d900d82a33deed6008731969058ff90a5c067
TheBrellis <brianroyalellis@gmail.com>
2019-12-09 19:44:36 -0500

    app now takes manager info and build correct html to teams array

--- app.js
+++ app.js
@@ -2,40 +2,12 @@ const inquirer = require("inquirer");
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
-
+const htmlBlocks = require("./lib/htmlBlocks.js")
 
 const team = [];
+team.push(htmlBlocks.header());
 
-async function initalize(role){
-  let answers1 = inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: `What is your ${role}'s name?`
-    },
-    {
-      type: "input",
-      name: "id",
-      message: `What is your ${role}'s ID?`
-    },
-    {
-      type: "input",
-      name: "email",
-      message: `What is your ${role}'s email?`
-    },
-    {
-      type: "list",
-      name: "role",
-      message:"What type of team member would you like to add?",
-      choices: ["Engineer","Intern","I don't want to add any more team members"]
-    }
-  ]);
-
-//let answers3 = {...answer1, ...answer2}
-//team.push(answers3)
-};
-
-function addMember(role){
+  /*
   if (role === "Engineer"){
     let answers2 = inquirer.prompt([
       {
@@ -55,12 +27,58 @@ function addMember(role){
     }
   ]) 
   }
-  return
-}
 
+  initalize(role)
+  let answers1 = inquirer.prompt();
+  let answer2 = inquirer.prompt([
+    {
+      type: "list",
+      name: "role",
+      message:"What type of team member would you like to add?",
+      choices: ["Engineer","Intern","I don't want to add any more team members"]
+    }
+    ]);
+    */
+function initalize(){
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: `What is your manager's name?`
+    },
+    {
+      type: "input",
+      name: "id",
+      message: `What is your manager's ID?`
+    },
+    {
+      type: "input",
+      name: "email",
+      message: `What is your manager's email?`
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: `What is your manager's office number?`
+    }
+  ])
+}
+function nextMember(){
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "role",
+      message:"What type of team member would you like to add?",
+      choices: ["Engineer","Intern","I don't want to add any more team members"]
+    }
+}
 /* ==========================================================*/
 //START OF APP SEQUENCE 
 /* ==========================================================*/
-console.log("Please build your team");s
-initalize("manager");
+initalize()
+.then((answers)=>{
+  const manager = new Manager(answers.name, answers.id, answers.email,answers.officeNumber);
+  team.push(manager.getHTML());
+  
+})
 
